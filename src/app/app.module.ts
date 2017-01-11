6dfdfecc45e84e855cfc5289b743316c68bf1f97
tushar_scaledesk <tushar@scaledesk.com>
2017-01-11 20:04:50 +0530

    add the TaskForm componentand the Task model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task.ts
@@ -0,0 +1,17 @@
+/**
+* @Author: Tushar Agarwal(tusharcoder) <tushar>
+* @Date:   2017-01-11T19:45:24+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: task.ts
+* @Last modified by:   tushar
+* @Last modified time: 2017-01-11T19:49:38+05:30
+*/
+
+
+
+export class Task {
+  constructor(
+    public title:string,
+    public description:string
+  ){}
+}
